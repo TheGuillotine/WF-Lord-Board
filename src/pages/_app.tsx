dc53d4a1746f73a5343c1a8308ed270ee9ca74e6
TheGuillotine <162359226+TheGuillotine@users.noreply.github.com>
2025-03-25 15:35:10 +0100

    Update _app.tsx to include map styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app';
 import { Analytics } from "@vercel/analytics/react";
 import '../styles/globals.css';
 import '../styles/enhanced-styles.css';
+import '../styles/stakers-map.css';
+import '../styles/stakers-map-extras.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
